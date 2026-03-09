1db2553e9d1193cea5951ebad1cfa68b362e6b26
rob1323 <mamangunrobert@itis-molinari.eu>
2026-03-09 23:02:11 +0100

    inizializza

---
+++ frontend/script.js
@@ -0,0 +1,35 @@
+const API = "http://localhost:3000/api/messages"
+
+function loadMessages() {
+
+fetch(API)
+.then(res => res.json())
+.then(data => {
+
+const list = document.getElementById("messages")
+list.innerHTML = ""
+
+data.forEach(msg => {
+const li = document.createElement("li")
+li.textContent = msg.text
+list.appendChild(li)
+})
+
+})
+
+}
+
+function sendMessage() {
+
+const text = document.getElementById("text").value
+
+fetch(API, {
+method: "POST",
+headers: { "Content-Type": "application/json" },
+body: JSON.stringify({ text: text })
+})
+.then(() => loadMessages())
+
+}
+
+loadMessages()
